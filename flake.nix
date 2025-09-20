5f7441aa3d3b569c007c6b48c17346d4de64d93f
ASTRA-SHC <astra@spacehardware.club>
2025-09-19 23:54:36 -0500

    clean up flake

--- flake.nix
+++ flake.nix
@@ -9,37 +9,33 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; }; 
+        pkgs = import nixpkgs { inherit system; };
         gst = pkgs.gst_all_1;
-        gstPlugins = [
-          gst.gst-plugins-base
-          gst.gst-plugins-good
-          gst.gst-plugins-bad
-          gst.gst-plugins-ugly
-          gst.gst-libav
+        gstPlugins = with gst; [
+          gst-plugins-base
+          gst-plugins-good
+          gst-plugins-bad
+          gst-plugins-ugly
+          gst-libav
         ];
-        gstPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (gstPlugins ++ [ gst.gstreamer ]);
+        gstPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+          (gstPlugins ++ [ gst.gstreamer ]);
       in {
-        packages.default = pkgs.stdenv.mkDerivation rec {
+        packages.default = pkgs.stdenv.mkDerivation {
           pname = "basestation-cameras";
           version = "0.1.0";
           src = ./.;
 
-          nativeBuildInputs = [
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.pkg-config
-            pkgs.qt6.wrapQtAppsHook
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            qt6.wrapQtAppsHook
           ];
 
-          buildInputs = [
-            pkgs.qt6.qtbase
-            pkgs.qt6.qttools
-            pkgs.qt6.qtnetworkauth
-            gst.gstreamer
-          ] ++ gstPlugins ++ [
-            pkgs.libunwind
-          ];
+          buildInputs = with pkgs;
+            [ qt6.qtbase qt6.qttools qt6.qtnetworkauth gst.gstreamer libunwind ]
+            ++ gstPlugins;
 
           cmakeFlags = [ "-G" "Ninja" ];
 
@@ -50,41 +46,52 @@
               --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "${gstPluginPath}" \
               --prefix GST_PLUGIN_PATH_1_0 : "${gstPluginPath}" \
               --set GIO_MODULE_DIR ${pkgs.glib.bin}/lib/gio/modules \
-              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [ pkgs.libGL pkgs.xorg.libX11 pkgs.xorg.libXi pkgs.xorg.libXext pkgs.xorg.libXrender pkgs.xorg.libxcb ]}
+              --prefix LD_LIBRARY_PATH : ${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.libGL
+                  pkgs.xorg.libX11
+                  pkgs.xorg.libXi
+                  pkgs.xorg.libXext
+                  pkgs.xorg.libXrender
+                  pkgs.xorg.libxcb
+                ]
+              }
           '';
         };
 
         apps.default = {
           type = "app";
-          program = "${self.packages.${system}.default}/bin/basestation-cameras";
+          program =
+            "${self.packages.${system}.default}/bin/basestation-cameras";
         };
 
         devShells.default = pkgs.mkShell {
           name = "basestation-cameras-dev";
-          buildInputs = [
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.pkg-config
-            pkgs.gcc
-            pkgs.gdb
+          buildInputs = with pkgs;
+            [
+              cmake
+              ninja
+              pkg-config
+              gcc
+              gdb
 
-            # Qt6
-            pkgs.qt6.qtbase
-            pkgs.qt6.qttools
-            pkgs.qt6.qtnetworkauth
-
-            # GStreamer + plugins
-            gst.gstreamer
-            gst.gst-plugins-base
-            gst.gst-plugins-good
-            gst.gst-plugins-bad
-            gst.gst-plugins-ugly
-            gst.gst-libav
-
-            # Useful tools
-            pkgs.ffmpeg
-            pkgs.libunwind
-          ];
+              # Useful tools
+              ffmpeg
+              libunwind
+            ] ++ (with qt6; [
+              # Qt6
+              qtbase
+              qttools
+              qtnetworkauth
+            ]) ++ (with gst; [
+              # GStreamer + plugins
+              gstreamer
+              gst-plugins-base
+              gst-plugins-good
+              gst-plugins-bad
+              gst-plugins-ugly
+              gst-libav
+            ]);
 
           shellHook = ''
             echo "[devShell] Qt6 + GStreamer environment ready"
@@ -95,8 +102,6 @@
             export GIO_MODULE_DIR="${pkgs.glib.bin}/lib/gio/modules"
           '';
         };
-      }
-    );
+      });
 }
-
 
